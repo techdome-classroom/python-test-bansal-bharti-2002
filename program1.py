deb714b95bef8d8b34ca820bc994ebdce0aff831
bharti-2002 <126313921+bharti-2002@users.noreply.github.com>
2024-02-23 13:11:54 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,12 +1,25 @@
-class Solution(object):
-    def isValid(self, s):
+class Solution:
+    def isValid(self, s:str)->bool:
+        stavk=[]
+        brackts_map={')':(','}':'{',']':'['}
+
+        for char in s:
+        if char in brackets_map.values():
+        stack.append(char)
+        if char in brakets_map.keys():
+        if not stack or
+        brakets_map[char]!=stack.pop():
+        return false
+        else:
+        return false
+
+        return len(stack)==0
+        
         """
-        :type s: str
+        :type s: str 
         :rtype: bool
         """
         pass
-    
-
 
 
   
